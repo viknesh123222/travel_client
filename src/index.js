42c19f02b2937b0a775436139957312c3c1314a8
vignesh <165367837+vigneshtpi@users.noreply.github.com>
2024-05-02 19:47:25 +0530

    push

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
 root.render(
   <React.StrictMode>
